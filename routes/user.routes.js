6a73671a093acdddbae602e4a50d023280bfe0d2
Miloš Mihić <milosmihic9@gmail.com>
2018-05-22 22:25:17 +0200

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,4 @@
-/**
- * @desc
- * Module provide routes for Create user, Retrive user/s, Update user and Delete user
- * @author
- * Created by mmihic (milos.mihic.17@singimail.rs) on 09.12.2017
- * @license
- * None
- */
+
 
 const express = require('express');
 const router = express.Router();
